fca86f15c28827ad29d427585b8d93439c1ad540
gyurim027 <dobby990207@gmail.com>
2026-09-16 16:42:39 +0900

    style: add base seller frontend design tokens

--- shopdb2_seller_frontend/src/main.jsx
+++ shopdb2_seller_frontend/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// 공통 디자인 변수 파일을 일반 CSS보다 먼저 불러옵니다.
+import './styles/tokens.css'
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
